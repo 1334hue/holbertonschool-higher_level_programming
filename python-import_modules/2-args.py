cdc76b548742a7b3248ee2fd3f1f10924414bf6f
Dana <trivian1966@gmail.com>
2026-06-06 04:14:39 +0000

    Modules

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    # Get the list of arguments excluding the script name
+    args = sys.argv[1:]
+    count = len(args)
+
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+
+    for i, arg in enumerate(args, start=1):
+        print("{}: {}".format(i, arg))
