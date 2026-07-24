dc275141b232d1ed800368fcd4f3984c5b3aadc3
dana <trivian1966@gmail.com>
2026-07-24 09:56:25 +0000

    sql

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,2 +1,3 @@
 --lists all databases on the MySQL server
+
 SHOW DATABASES;
--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,1 +1,2 @@
+--updates the score of Bod to 10 in second_table
 UPDATE second_table SET score = 10 WHERE name = "Bob";
